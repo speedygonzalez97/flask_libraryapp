6b9714d0877dbc748e3791d105cf59f7c6657516
Bryan Gonzalez <gonzalez.bryan9@gmail.com>
2023-11-16 01:32:15 -0500

    library project flask

---
+++ app/api/routes.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Contact, contact_schema, contacts_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return {'yee': 'haw'}
+
+@api.route('/contacts', methods = ['POST'])
+@token_required
+def create_contact(current_user_token):
+    isbn = request.json['isbn']
+    title = request.json['title']
+    author_name = request.json['author_name']
+    book_length = request.json['book_length']
+    cover_type = request.json['cover_type']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    contact = Contact(isbn, title, author_name, book_length, cover_type, user_token = user_token )
+
+    db.session.add(contact)
+    db.session.commit()
+
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts', methods = ['GET'])
+@token_required
+def get_contact(current_user_token):
+    a_user = current_user_token.token
+    contacts = Contact.query.filter_by(user_token = a_user).all()
+    response = contacts_schema.dump(contacts)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['GET'])
+@token_required
+def get_single_contact(current_user_token, id):
+    contact = Contact.query.get(id)
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['POST','PUT'])
+@token_required
+def update_contact(current_user_token,id):
+    contact = Contact.query.get(id) 
+    contact.isbn = request.json['isbn']
+    contact.title = request.json['title']
+    contact.author_name = request.json['author_name']
+    contact.book_length = request.json['book_length']
+    contact.cover_type = request.json['cover_type']
+    contact.user_token = current_user_token.token
+
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['DELETE'])
+@token_required
+def delete_contact(current_user_token, id):
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
